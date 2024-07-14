22612afce4a53035f563dca9c75f428bcc02c7a3
jso9603 <jso9603@gmail.com>
2024-07-14 17:38:34 +0900

    확률 높은 당첨번호로 번호 추첨 기능 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,14 @@
   <div class="kakao__login">
     <KakaoLogin />
   </div>
+
+  <div class="high-probability">
+    <div>확률 높은 번호 추첨</div>
+    <button @click="generateHighNumbers">확률 높은 번호 추첨</button>
+    <li v-for="(set, index) in lotterySets" :key="index">
+      {{ index + 1 }}회차: {{ set.join(', ') }}
+    </li>
+  </div>
 </div>
   
 </template>
@@ -34,13 +42,55 @@ export default class Home extends Vue {
   lottoNumbers: number[][] = []
   winningMsg: string = ''
 
+  // 확률 높은 숫자를 숫자 번대로 2개씩 선택
+  highProbNumbers: number[] = [1, 3, 12, 17, 26, 27, 33, 34, 42, 45]
+  lotterySets: number[][] = []
+
+ // Function to generate a single set of lottery numbers
+  generateLotteryNumbers(): number[] {
+    const selectedHighProbNumbers = this.getRandomNumbers(this.highProbNumbers, 3)
+    const remainingNumbers = this.getRemainingNumbers(selectedHighProbNumbers)
+    const selectedRemainingNumbers = this.getRandomNumbers(remainingNumbers, 3)
+    return selectedHighProbNumbers.concat(selectedRemainingNumbers).sort((a, b) => a - b)
+  }
+
+  // Helper function to get random numbers from an array
+  getRandomNumbers(array: number[], count: number): number[] {
+    const result = []
+    const _array = [...array]
+    for (let i = 0; i < count; i++) {
+      const index = Math.floor(Math.random() * _array.length)
+      result.push(_array.splice(index, 1)[0])
+    }
+    return result
+  }
+
+  // Helper function to get the remaining numbers
+  getRemainingNumbers(exclude: number[]): number[] {
+    const remaining = []
+    for (let i = 1; i <= 45; i++) {
+      if (!exclude.includes(i) && !this.highProbNumbers.includes(i)) {
+        remaining.push(i)
+      }
+    }
+    return remaining
+  }
+
+  // Function to generate 5 sets of lottery numbers
+  generateHighNumbers() {
+    this.lotterySets = [];
+    for (let i = 0; i < 5; i++) {
+      this.lotterySets.push(this.generateLotteryNumbers())
+    }
+  }
+
   generateNumbers() {
     const rounds = 5 // 5회차
     this.lottoNumbers = []
 
     for (let i = 0; i < rounds; i++) {
       const numbers = new Set<number>()
-      while (numbers.size < 6) {
+      while (numbers.size < 7) {
         const randomNum = Math.floor(Math.random() * 45) + 1
         numbers.add(randomNum)
       }
