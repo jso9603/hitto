b6c27755bfcef3fbf81633f9a3c19af47ea4ddbc
jso9603 <jso9603@gmail.com>
2024-08-18 17:31:14 +0900

    설정 페이지, 로그아웃 기능 추가

---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="toggle-switch" @click="toggle">
+    <div class="toggle-track" :class="{ 'is-checked': isChecked }">
+      <div class="toggle-thumb"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ToggleSwitch extends Vue {
+  private isChecked: boolean = true
+
+  private toggle(): void {
+    this.isChecked = !this.isChecked;
+    this.$emit('change', this.isChecked);
+  }
+}
+</script>
+
+<style scoped>
+.toggle-switch {
+  width: 50px;
+  height: 25px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.toggle-track {
+  width: 100%;
+  height: 100%;
+  background-color: #2e2e2e;
+  border-radius: 25px;
+  position: relative;
+  transition: background-color 0.3s ease;
+  border: 2px solid #5F6163;
+}
+
+.toggle-track.is-checked {
+  background-color: red;
+}
+
+.toggle-thumb {
+  width: 20px;
+  height: 20px;
+  background-color: #4AFF81;
+  border-radius: 50%;
+  position: absolute;
+  top: 2px;
+  left: 1px;
+  transition: transform 0.3s ease;
+}
+
+.toggle-track.is-checked .toggle-thumb {
+  transform: translateX(24px);
+}
+</style>
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -2,29 +2,121 @@
   <div class="setting">
     <div class="menu">
       <div class="label">
-        <div class="title">연결된 계정</div>
+        <div class="title">
+          연결된 계정
+          <div class="email" v-if="user">
+            <div class="kakao">
+              <img src="@/assets/ic-system-kakao.svg" />
+            </div>
+            {{user.email}}
+          </div>
+        </div>
       </div>
-      <div class="content"></div>
+      <div class="contents">
+        <ToggleSwitch @change="handleToggle"></ToggleSwitch>
+      </div>
     </div>
+
+    <div class="leave" @click="onLeave">탈퇴하기</div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Cookies from 'js-cookie'
+import ToggleSwitch from '@/components/ToggleSwitch.vue'
+import { getLoggedUserInfo } from '@/utils/user'
 
-@Component
+@Component({
+  components: {
+    ToggleSwitch,
+  }
+})
 export default class Setting extends Vue {
-  
+  user: any | null = null
+  private toggleState: boolean = false
+
+  private async handleToggle(newState: boolean) {
+    this.toggleState = newState;
+
+    if (!this.toggleState) {
+      await Cookies.remove('user');
+      await alert('로그아웃 되었습니다');
+      await this.$router.push('/');
+    }
+  }
+
+  onLeave() {
+    // 페이지 이동
+  }
+
+  created() {
+    const user = getLoggedUserInfo();
+    if (user) {
+      this.user = user;
+    }
+  }
 }
 </script>
 
 <style scoped>
 .setting {
   padding: 24px 20px;
+  position: relative;
+  height: calc(100vh - 102px);
 }
 
 .menu {
   display: flex;
-  align-items: cent4er;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 24px;
+}
+
+.menu > .label {
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 26px;
+  color: #ECEEF0;
+}
+
+.menu > .label > .title {
+  margin-bottom: 4px;
+}
+
+.menu > .label > .title > .email {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  color: #9C9EA0;
+}
+
+.menu > .label > .title > .email > .kakao {
+  background-color: #ECEEF0;
+  width: 18px;
+  height: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 75px;
+}
+
+.kakao > img {
+  width: 13.5px;
+  height: 13.5px;
+}
+
+.leave {
+  position: absolute;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 21px;
+  color: #9C9EA0;
+  text-decoration: underline;
+  bottom: calc(20px + env(safe-area-inset-bottom));
+  cursor: pointer;
 }
 </style>
